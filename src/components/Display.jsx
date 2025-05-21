15db56c6e7770b310663b1c17a712fa2c3dd39ef
rip-the-slit <171191896+rip-the-slit@users.noreply.github.com>
2025-05-21 22:32:05 +0000

    create general information section in the Display component

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,5 +1,13 @@
-export default function Display({data}) {
-    return <section>
-        {data.name}
+export default function Display({ data }) {
+  return <main>
+    <section id="general-information">
+        <h1>{data.name}</h1>
+        <address>
+            <ul>
+                <li><a href={"mailto:" + data.email}>{data.email}</a></li>
+                <li><a href={"tel:" + data.phone}>{data.phone}</a></li>
+            </ul>
+        </address>
     </section>
+  </ main>;
 }
