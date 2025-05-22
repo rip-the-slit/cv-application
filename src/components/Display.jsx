26587f1eaca6d6e3e9ab014572806b30909b69ce
rip-the-slit <171191896+rip-the-slit@users.noreply.github.com>
2025-05-22 15:25:56 +0000

    create educational experience section in the Display component

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,13 +1,29 @@
 export default function Display({ data }) {
-  return <main>
-    <section id="general-information">
+  return (
+    <main>
+      <section id="general-information">
         <h1>{data.name}</h1>
         <address>
-            <ul>
-                <li><a href={"mailto:" + data.email}>{data.email}</a></li>
-                <li><a href={"tel:" + data.phone}>{data.phone}</a></li>
-            </ul>
+          <ul>
+            <li>
+              <a href={"mailto:" + data.email}>{data.email}</a>
+            </li>
+            <li>
+              <a href={"tel:" + data.phone}>{data.phone}</a>
+            </li>
+          </ul>
         </address>
-    </section>
-  </ main>;
+      </section>
+      <section id="educational-experience">
+        <h2>Educational Experience</h2>
+        <ul>
+          <li>School: {data.school}</li>
+          <li>Title of Study: {data.studyTitle}</li>
+          <li>
+            Date of Study: {data.studyDate.from} - {data.studyDate.to}
+          </li>
+        </ul>
+      </section>
+    </main>
+  );
 }
